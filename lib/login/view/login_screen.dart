136da15e17ef6a4682f7c0fc6928ac973d8ef451
Danielcar2na <danielcar2na@gmail.com>
2024-10-18 16:44:22 -0500

    se quita expaded de boton login

--- lib/login/view/login_screen.dart
+++ lib/login/view/login_screen.dart
@@ -14,91 +14,87 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(250, 0, 0, 61),
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Spacer(flex: 1),
-                        _buildTitle(),
-                        SizedBox(height: constraints.maxHeight * 0.05),
-                        _buildLogo(constraints),
-                        SizedBox(height: constraints.maxHeight * 0.05),
-                        _buildUserField(),
-                        SizedBox(height: 10),
-                        _buildPasswordField(),
-                        SizedBox(height: 20),
-                        _buildLoginButton(constraints),
-                        Spacer(flex: 2),
-                      ],
-                    ),
-                  ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Agregar el título aquí
+              const Text(
+                'Inventory Manager',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            );
-          },
+              SizedBox(height: 50,),
+              logo(),
+              campoUsuario(),
+              campoContrasena(),
+              btnEntrar()
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildTitle() {
-    return Text(
-      'Inventory Manager',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 28,
-        fontWeight: FontWeight.bold,
-      ),
-      textAlign: TextAlign.center,
-    );
+  Widget singIn() {
+    return const Text('Sign in',
+        style: TextStyle(color: Colors.white, fontSize: 35));
   }
 
-  Widget _buildLogo(BoxConstraints constraints) {
-    return SizedBox(
-      height: constraints.maxHeight * 0.2,
-      child: Image.asset(
-        'assets/img/description.png',
-        fit: BoxFit.contain,
+  Widget campoUsuario() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 2),
+      child: CustomTextField(
+        userEditingController: viewModel.userTextField,
+        prefixIconData: Icons.person,
+        hint: 'Usuario',
+        maxLenght: 20,
       ),
     );
   }
 
-  Widget _buildUserField() {
-    return CustomTextField(
-      userEditingController: viewModel.userTextField,
-      prefixIconData: Icons.person,
-      hint: 'Usuario',
-      maxLenght: 20,
+  Widget campoContrasena() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 2),
+      child: CustomTextField(
+        userEditingController: viewModel.passwordTextField,
+        prefixIconData: Icons.lock,
+        hint: 'Contraseña',
+        maxLenght: 20,
+        obscure: true,
+      ),
     );
   }
 
-  Widget _buildPasswordField() {
-    return CustomTextField(
-      userEditingController: viewModel.passwordTextField,
-      prefixIconData: Icons.lock,
-      hint: 'Contraseña',
-      maxLenght: 20,
-      obscure: true,
-    );
+  Widget btnEntrar() {
+    return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        width: Get.width,
+        //height: Get.height * 0.09,
+        child: CustomButton(
+            backgroundColor: Colors.white,
+            onPressed: () {
+              viewModel.login();
+            },
+            text: 'Iniciar Sesión'));
   }
 
-  Widget _buildLoginButton(BoxConstraints constraints) {
+  Widget logo() {
     return SizedBox(
-      width: constraints.maxWidth * 0.8,
-      child: CustomButton(
-        backgroundColor: Colors.white,
-        onPressed: () {
-          viewModel.login();
-        },
-        text: 'Iniciar Sesión',
+      height: 180,
+      width: 200,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/img/description.png',
+            width: 130,
+          ),
+        ],
       ),
     );
   }
